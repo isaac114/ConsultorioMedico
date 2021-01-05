1311fe661200a5fe56159895523c3b44188a9b54
jessica-maribel <jessicaguncay45@gmail.com>
2021-01-05 04:48:15 -0500

    Cita Medica

---
+++ src/entidades/CitaMedica.java
@@ -0,0 +1,22 @@
+package entidades;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: CitaMedica
+ * 
+ */
+
+@Entity 
+public class CitaMedica implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public CitaMedica() {
+		super();
+	}
+   
+}
--- src/entidades/ConsultaClinica.java
+++ src/entidades/ConsultaClinica.java
@@ -1,6 +1,7 @@
 package entidades;
 
 import java.io.Serializable;
+
 import java.util.List;
 
 import javax.persistence.*;

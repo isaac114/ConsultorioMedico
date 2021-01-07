ffc1fa85032ee2c5fe1c9870fa79934eee3b7adf
jessica-maribel <jessicaguncay45@gmail.com>
2021-01-07 18:22:24 -0500

    cambios

--- src/entidades/CitaMedica.java
+++ src/entidades/CitaMedica.java
@@ -16,6 +16,7 @@ public class CitaMedica implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private Date fecha;
 	private String nombreSala;

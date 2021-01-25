95123fc459fe2209922db0f0c024ddc23ad972ec
isaac114 <isaac.chavez.114@gmail.com>
2021-01-24 21:23:38 -0500

    -

--- src/jpa/JPAConsultaClinicaDAO.java
+++ src/jpa/JPAConsultaClinicaDAO.java
@@ -1,5 +1,7 @@
 package jpa;
 
+import javax.persistence.Query;
+
 import dao.ConsultaClinicaDAO;
 import entidades.ConsultaClinica;
 
@@ -8,6 +10,23 @@ public class JPAConsultaClinicaDAO extends JPAGenericDAO<ConsultaClinica, Intege
 	public JPAConsultaClinicaDAO() {
 		super(ConsultaClinica.class);
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int obtenerUltimaConsulta(int idPaciente) {
+		// TODO Auto-generated method stub
+		Query query = em.createQuery("Select max(a.codigo) FROM ConsultaClinica a");
+		int id = (int) query.getSingleResult();
+		return id;
+	}
+
+	@Override
+	public ConsultaClinica buscarPorID(int idConsulta) {
+		// TODO Auto-generated method stub
+		Query query = em.createQuery("Select a FROM ConsultaClinica a WHERE a.codigo = :codigo");
+		query.setParameter("codigo", idConsulta);
+		ConsultaClinica cc = (ConsultaClinica) query.getSingleResult();
+		return cc;
 	}
 
 }
